e4f913eb1d2af0385707a6129a133f20a6755a76
Your Name <your_email@whatever.com>
2020-06-23 21:35:51 +0300

    delete button

--- index.js
+++ index.js
@@ -9,14 +9,20 @@ form.addEventListener("submit", function (event) {
   newDiv.className = "marker"
   let span = document.createElement("span");
   let button = document.createElement("button");
+  let cross = document.createElement("span");
+  cross.innerHTML="X";
+  cross.className = "delete"
+  cross.addEventListener('click', function () {
+    newDiv.remove()
+  })
   button.addEventListener('click', function () {
-
-  span.classList.toggle('lines')  
+    span.classList.toggle('lines')
   }) 
   span.innerHTML = firstInput.value;
   button.innerHTML= "mark as done"
   newDiv.appendChild(span)
-  newDiv.appendChild(button);
+  newDiv.appendChild(button)
+  newDiv.appendChild(cross);
   if (firstInput.value !== "") {
     firstInput.value = "";
     wrapper.appendChild(newDiv);
