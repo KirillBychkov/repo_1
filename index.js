dfcf1c6549cc2b3493247b8bb866bc56305c1ec3
Viktor <0510kollin@gmail.com>
2020-06-23 20:21:14 +0300

    create function for button

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ let wrapper = document.querySelector('#wrapper')
 console.log(wrapper)
 wrapper.innerHTML = "text"
 
+
+let form = document.querySelector("form.firstBlock")
+
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    let firstInput = document.querySelector(".text");
+    console.log(firstInput.value);
+    firstInput.value = "";
+})
