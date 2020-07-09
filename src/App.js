ea7125d98a8a135659ac4291ad61ff9c85f5b127
Your Name <your_email@whatever.com>
2020-07-09 19:42:47 +0300

    add list properties

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { useState } from "react";
 import Button from "./components/button";
 import Input from "./components/input";
 import List from "./components/list";
-import { initInputValue } from "./const";
+import { initInputValue, list } from "./const";
 
 import "./style.sass";
 
 const App = () => {
   const [value, setValue] = useState(initInputValue);
+
+  const [myList, setList] = useState(list)
 
   const addTask = () => {
     console.log(value);
@@ -20,7 +22,7 @@ const App = () => {
         <Button text="click me" onclick={addTask} />
         <Input placeholder="enter text" value={value} setValue={setValue} />
       </div>
-      <List />
+      <List list={myList} />
     </div>
   );
 }
--- src/components/list/index.js
+++ src/components/list/index.js
@@ -1,7 +1,6 @@
 import React from "react";
-import { list } from "../../const";
 
-const List = () =>
+const List = ({ list }) =>
   <ul>
     {
       list.map(
